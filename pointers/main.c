0215e1727f5359385379cc26a565a33bbbd0c2b0
sriram-jeganathan <sriram.jeganathan@outlook.com>
2025-06-16 19:45:09 +0530

    Update main.c

--- pointers/main.c
+++ pointers/main.c
@@ -2,33 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-struct student{
-	int rollno;
-	char name[50];
-	char school[50];
-	char grade[10];
+struct student {
+    int rollno;
+    char name[50];
+    char school[50];
+    char grade[10];
 };
 
-
-
-
 int main() {
-	struct student rec1;
+    struct student rec1;
 
-	printf("Enter rollNo: ");
-	scanf("%d", &rec1.rollno);
+    printf("Enter rollNo: ");
+    scanf("%d", &rec1.rollno);
+    getchar();  // Consume leftover newline
 
-	printf("Enter name: ");
-	scanf("%c", &rec1.name);
+    printf("Enter name: ");
+    scanf(" %[^\n]s", rec1.name);
 
-	printf("Enter school: ");
-	scanf("%c", &rec1.school);
+    printf("Enter school: ");
+    scanf(" %[^\n]s", rec1.school);
 
-	printf("Enter grade: ");
-	scanf("%c", &rec1.grade);
+    printf("Enter grade: ");
+    scanf(" %[^\n]s", rec1.grade);
 
-	printf("rollno: %d\nname: %s\nschool: %s\ngrade: %s\n", rec1.rollno, rec1.name, rec1.school, rec1.grade);
+    printf("\nStudent Record:\n");
+    printf("Roll No: %d\n", rec1.rollno);
+    printf("Name: %s\n", rec1.name);
+    printf("School: %s\n", rec1.school);
+    printf("Grade: %s\n", rec1.grade);
 
-	return 0;
+    return 0;
 }
