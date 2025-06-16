f063bc6a8ad441c356634123d20d481ab2af561f
sriram-jeganathan <sriram.jeganathan@outlook.com>
2025-06-16 20:47:03 +0530

    Update main.c

--- pointers/main.c
+++ pointers/main.c
@@ -11,25 +11,26 @@ struct student {
 
 int main() {
     struct student rec1;
+    struct student *ptr = &rec1;
 
     printf("Enter rollNo: ");
-    scanf("%d", &rec1.rollno);
-    getchar();  // Consume leftover newline
+    scanf("%d", &(*ptr).rollno);
+    getchar(); 
 
     printf("Enter name: ");
-    scanf(" %[^\n]s", rec1.name);
+    scanf(" %[^\n]s", (*ptr).name);
 
     printf("Enter school: ");
-    scanf(" %[^\n]s", rec1.school);
+    scanf(" %[^\n]s", (*ptr).school);
 
     printf("Enter grade: ");
-    scanf(" %[^\n]s", rec1.grade);
+    scanf(" %[^\n]s", (*ptr).grade);
 
     printf("\nStudent Record:\n");
-    printf("Roll No: %d\n", rec1.rollno);
-    printf("Name: %s\n", rec1.name);
-    printf("School: %s\n", rec1.school);
-    printf("Grade: %s\n", rec1.grade);
+    printf("Roll No: %d\n", (*ptr).rollno);
+    printf("Name: %s\n", (*ptr).name);
+    printf("School: %s\n", (*ptr).school);
+    printf("Grade: %s\n", (*ptr).grade);
 
     return 0;
 }
