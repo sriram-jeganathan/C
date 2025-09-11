9fd2a8cea7a194941b1779b5e2af713aab542302
sriram-jeganathan <sriram.jeganathan@outlook.com>
2025-09-11 10:29:34 +0530

    updated files

---
+++ test/basic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main ( void ) {
+    int x;
+    printf ( "Enter A Number: " );
+    scanf ( "%d", &x );
+    printf ( "%d is the value of x\n", x );
+    
+    return;
+}
